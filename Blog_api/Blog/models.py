f88d5632122ed6cbde0c13b4cf6117e78287c154
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2020-10-04 22:09:28 +0200

    custom manager to Entry model

--- Blog_api/Blog/models.py
+++ Blog_api/Blog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Entry(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField()
-    created = models.DateTimeField(null=True)
-    modified = models.DateTimeField(null=True)
-    pub_date = models.DateTimeField(null=True)
+    created = models.DateTimeField(null=True,auto_now=True)
+    modified = models.DateTimeField(null=True,auto_now=True)
+    pub_date = models.DateTimeField(null=True,auto_now_add=True)
     comments_count = models.IntegerField()
