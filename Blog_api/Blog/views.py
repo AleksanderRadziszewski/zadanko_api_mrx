e6ef2baddc1729000fd039e8ed671d5714eda2de
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2020-10-04 21:53:11 +0200

    Article  model

--- Blog_api/Blog/views.py
+++ Blog_api/Blog/views.py
@@ -1,3 +1,21 @@
+from django.http import request
 from django.shortcuts import render
+from django.views import View
+
+from Blog.forms import EntryForm
+from Blog.models import Entry
+
+
+class EntryView(View):
+    def get(self,request):
+        form=EntryForm()
+        return render(request, "Blog/form.html", {"form":form})
+    def post(self,request):
+        form=EntryForm(request.POST)
+        if form.is_valid():
+            Entry.objects.create(**form.cleaned_data)
+        return render(request, "Blog/form.html", {"form":form})
+
+
 
 # Create your views here.
