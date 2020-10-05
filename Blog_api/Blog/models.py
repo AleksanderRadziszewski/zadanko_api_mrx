25da359be4cb4babbf94b9b9ce55d3e3c227d16d
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2020-10-05 14:58:03 +0200

    Generic realtionship between Comment and Entry,Comment and Article

--- Blog_api/Blog/models.py
+++ Blog_api/Blog/models.py
@@ -7,4 +7,6 @@ class Entry(models.Model):
     created = models.DateTimeField(null=True,auto_now=True)
     modified = models.DateTimeField(null=True,auto_now=True)
     pub_date = models.DateTimeField(null=True,auto_now_add=True)
-    comments_count = models.IntegerField()
+    comments_count = models.IntegerField(null=True)
+
+
