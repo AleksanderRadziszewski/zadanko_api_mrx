e02f4f1a6ee26856389c1e7b05d08a5fc27d2677
Aleksander Radziszewski <radziszewski.aleksander@gmail.com>
2020-11-19 18:19:59 +0100

    deleted Blog_api

--- Blog_api/Blog/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-from blog.models import Entry
-
-
-class EntryForm(forms.ModelForm):
-    class Meta:
-        model = Entry
-        exclude = ["comments_count"]
--- Blog_api/Comments/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from comments.models import Comments
-
-
-class AddCommentForm(forms.ModelForm):
-
-    class Meta:
-        model = Comments
-        fields = "__all__"
